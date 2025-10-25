a9416837af263f169740f02ecc1b4893683e9dab
rkravihhh <rkravichennai7@outlook.com>
2025-10-25 07:32:46 +0530

    Delete infra directory

--- infra/envs/_backend/terraform.tfvars
+++
@@ -1,8 +0,0 @@
-bucket_name = "fintech-tfstate-dev"
-region      = "us-east-1"
-
-tags = {
-  ManagedBy = "Terraform"
-  Purpose   = "RemoteState"
-  Environment = "Backend"
-}
--- infra/envs/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "fintech-tfstate-dev"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    use_lockfile   = true
-  }
-}
